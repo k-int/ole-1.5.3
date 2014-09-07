9c66ec59c95b61d24c712906594c97ba9f831abd
jcaddel <jcaddel@0598a23e-c0cf-4bbf-8d85-e8311fe397aa>
2014-09-07 11:22:32 +0000

    [maven-release-plugin]  copy for tag ole-1.5.3

---
+++ tags/ole-1.5.3/ole-docstore/ole-docstore-engine/src/main/java/org/kuali/ole/docstore/engine/service/index/solr/DocumentIndexer.java
@@ -0,0 +1,33 @@
+package org.kuali.ole.docstore.engine.service.index.solr;
+
+import org.apache.solr.client.solrj.SolrServerException;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: sambasivam
+ * Date: 12/17/13
+ * Time: 4:52 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DocumentIndexer {
+
+    public void create(Object object);
+
+    public void update(Object object);
+
+    public void delete(String id);
+
+    public void transfer(List<String> sourceIds, String destinationId);
+
+    public void bind(String holdingsId, List<String> bibIds) throws SolrServerException, IOException;
+
+    public void bindAnalytics(String holdingsId, List<String> bibIds, String createOrBreak) throws SolrServerException, IOException;
+
+    public void createTree(Object object);
+
+    public void createTrees(Object object);
+
+}
